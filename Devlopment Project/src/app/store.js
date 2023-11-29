4ad66c49d5b004c43a27a8317ef6993e8fb3a3f4
Shivam <121795131+shivu1319@users.noreply.github.com>
2023-11-29 14:21:14 +0530

    Delete Devlopment Project/src/app directory

--- Devlopment Project/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore,applyMiddleware } from "redux";
-import reduxThunk from 'redux-thunk' // for middleware for api serve
-import rootReducer from "./rootReducer";
-
-const thunkmiddleware=[reduxThunk];
-
-const store=createStore(rootReducer,applyMiddleware(...thunkmiddleware));
-
-export default store;
